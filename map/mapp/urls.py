8207c0ba76775ac07906235c2fa80ed6347b7a68
BAHE4KA1 <rogovi193@gmail.com>
2024-12-08 13:47:18 +0300

    7

--- map/mapp/urls.py
+++ map/mapp/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('request/', views.requ, name='requ'),
     path('analitics/', views.analitics, name='analitic'),
-    path('home/', views.home, name='home'),
-    path('', views.map, name='map'),
+    path('', views.home, name='home'),
+    path('map/', views.map, name='map'),
 ]
